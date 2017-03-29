b6197063e1bc2aa0620f08689afe48a7270f1705
Peter Arthur <pete.arthur@gmail.com>
2017-03-29 21:39:23 +0100

    Add beginnings of a test project

---
+++ AcuCafeTests/AcuCafeTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AcuCafeTests
+{
+    using AcuCafe;
+
+    [TestClass]
+    public class AcuCafeTests
+    {
+        [TestMethod]
+        public void TestOrderDrink()
+        {
+            AcuCafe.OrderDrink("Espresso", false, false);
+        }
+    }
+}
